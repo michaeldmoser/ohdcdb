ccd4be79ce6717bb0854402b0cd6ed2c27952ff6
Michael Moser <michaeldmoser@gmail.com>
2022-01-07 14:30:55 +0000

    Create more frontend files

---
+++ ohdccrm/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react';
+
+class App extends Component
+{
+    render() {
+        return <h1>Orchard Homes Ditch Company CRM</h1>
+    }
+}
+
+ReactDOM.redner(<App />, document.getElementById('app'));
