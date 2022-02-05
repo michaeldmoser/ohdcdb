cb0e549001163904606812268671b3a9d123d972
Michael Moser <michaeldmoser@gmail.com>
2022-02-05 14:20:05 +0000

    Doing a reset of things again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,90 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <div id='layoutAuthentication'>
-                <div id='layoutAuthentication_content'>
-                    <main>
-                        <div className='container'>
-                            <div className='row justify-content-center'>
-                                <div className='col-lg-5'>
-                                    <div className='card shadow-lg border-0 rounded-lg mt-5'>
-                                        <div className='card-header'>
-                                            <h3 className='text-center font-weight-light my-4'>
-                                                Login
-                                            </h3>
-                                        </div>
-                                        <div className='card-body'>
-                                            <form>
-                                                <div className='form-floating mb-3'>
-                                                    <input
-                                                        className='form-control'
-                                                        id='inputEmail'
-                                                        type='email'
-                                                        placeholder='name@example.com'
-                                                        name='email'
-                                                    />
-                                                    <label htmlFor='inputEmail'>
-                                                        Email address
-                                                    </label>
-                                                </div>
-                                                <div className='form-floating mb-3'>
-                                                    <input
-                                                        className='form-control'
-                                                        id='inputPassword'
-                                                        type='password'
-                                                        placeholder='Password'
-                                                        name='current-password'
-                                                    />
-                                                    <label htmlFor='inputPassword'>
-                                                        Password
-                                                    </label>
-                                                </div>
-                                                <div className='d-flex align-items-center justify-content-between mt-4 mb-0'>
-                                                    <a
-                                                        className='small'
-                                                        href='password.html'
-                                                    >
-                                                        Forgot Password?
-                                                    </a>
-                                                    <a
-                                                        className='btn btn-primary'
-                                                        href='index.html'
-                                                    >
-                                                        Login
-                                                    </a>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div className='card-footer text-center py-3'>
-                                            <div className='small'>Hi!</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </main>
-                </div>
-                <div id='layoutAuthentication_footer'>
-                    <footer className='py-4 bg-light mt-auto'>
-                        <div className='container-fluid px-4'>
-                            <div className='d-flex align-items-center justify-content-between small'>
-                                <div className='text-muted'>
-                                    Copyright &copy; ZooTown Apps 2022
-                                </div>
-                                <div>
-                                    <a href='#'>Privacy Policy</a>
-                                    &middot;
-                                    <a href='#'>Terms &amp; Conditions</a>
-                                </div>
-                            </div>
-                        </div>
-                    </footer>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
