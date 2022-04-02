9851b9bdd74bc25b6f85b2638c5f39ea50c9de09
Michael Moser <michaeldmoser@gmail.com>
2022-04-02 13:26:23 +0000

    Hide list on small screens

--- frontend/src/features/people/index.js
+++ frontend/src/features/people/index.js
@@ -1,7 +1,13 @@
 import './people.scss';
 import { useState } from 'react';
 
-import { Route, Routes } from 'react-router-dom';
+import {
+    Route,
+    Routes,
+    useLocation,
+    useMatch,
+    matchPath,
+} from 'react-router-dom';
 
 import PeopleList from './PeopleList';
 import PersonDetail from './PersonDetail';
@@ -9,7 +15,34 @@ import PeopleHeader from './PeopleHeader';
 import AddPerson from './AddPerson';
 import EditPerson from './EditPerson';
 
-const People = () => {
+const ListOnly = ({ query }) => {
+    return (
+        <>
+            <div className='col-md-3 col-12'>
+                <PeopleList query={query} />
+            </div>
+        </>
+    );
+};
+
+const ListWithDetails = ({ query }) => {
+    return (
+        <>
+            <div className='col-3 d-none d-lg-block'>
+                <PeopleList query={query} />
+            </div>
+            <div className='col-12 col-lg-9'>
+                <Routes>
+                    <Route path='add-person' element={<AddPerson />} />
+                    <Route path=':personId' element={<PersonDetail />} />
+                    <Route path=':personId/edit' element={<EditPerson />} />
+                </Routes>
+            </div>
+        </>
+    );
+};
+
+const People = ({ rootPath }) => {
     const [query, setSearchQuery] = useState('');
 
     return (
@@ -17,22 +50,13 @@ const People = () => {
             <PeopleHeader query={query} setSearchQuery={setSearchQuery} />
             <div className='card'>
                 <div className='row'>
-                    <div className='col-3'>
-                        <PeopleList query={query} />
-                    </div>
-                    <div className='col-9'>
-                        <Routes>
-                            <Route path='add-person' element={<AddPerson />} />
-                            <Route
-                                path=':personId'
-                                element={<PersonDetail />}
-                            />
-                            <Route
-                                path=':personId/edit'
-                                element={<EditPerson />}
-                            />
-                        </Routes>
-                    </div>
+                    <Routes>
+                        <Route index element={<ListOnly query={query} />} />
+                        <Route
+                            path='*'
+                            element={<ListWithDetails query={query} />}
+                        />
+                    </Routes>
                 </div>
             </div>
         </section>
